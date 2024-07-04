84ea6a89018603fed662d3fdf049a28fd0322066
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-04 12:34:39 +0200

    build Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,13 @@
+import logo from "../assets/desktop/logo.svg";
+
 const NavBar = () => {
-  return <div className="text-4xl font-bold text-light_violet">NavBar</div>;
+  return (
+    <div className="nav">
+      <div className="max-container">
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
