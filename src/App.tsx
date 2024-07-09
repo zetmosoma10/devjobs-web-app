c0d7a91be1c0f0afe23beaa365f9825347ce42a3
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-07-09 13:50:15 +0200

    implement scroll-top component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import JobDetailsPage from "./pages/JobDetailsPage";
+import ScrollTop from "./components/ScrollTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollTop />
       <Routes>
         <Route path="/" element=<Layout />>
           <Route index element=<HomePage /> />
